156d68d9f9a894a866710956117ec829bba01d8a
João V S Pessin <67722165+joaopessin@users.noreply.github.com>
2022-08-23 11:58:02 -0300

    Update ExerciciosPlanejados12.java

--- ExerciciosPlanejados12.java
+++ ExerciciosPlanejados12.java
@@ -16,4 +16,7 @@
     2_ Pergunte ao aluno quantas notas ele deseja calcular, usa o "for" para ler todas as notas e depois calcule e exiba a média final do aluno.
     Também mostre se ele foi aprovado (media 7), reprovado direto (menor que 4) ou ficou de recuperação.
     Se ele ficou de recuperação, pergunte a nota da prova de recuperação, calcule a média final + nota da prova de recuperação e mostre se ele foi aprovado ou não.
+    
+   Exercício Profissional:
+    1_ Pergunte a quantidade de pacientes, leia o nome de cada um usando o "for" e armazene em um vetor. Exiba o nome de cada paciente no final.
 */
