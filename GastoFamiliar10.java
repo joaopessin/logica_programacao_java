f3f00b4853788fbcfbdad351e6827ce6534a9ede
João V S Pessin <67722165+joaopessin@users.noreply.github.com>
2022-08-22 21:24:57 -0300

    Update GastoFamiliar10.java

--- GastoFamiliar10.java
+++ GastoFamiliar10.java
@@ -6,25 +6,26 @@ public class GastoFamiliar2 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.print("Informe o valor da conta de luz: ");
+        float somaTotalGastos = 0.0F;
+
+        System.out.println("Informe os valores gastos com...\n");
+        System.out.print("Conta de luz: ");
         float contaLuz = entrada.nextFloat();
 
-        System.out.print("Informe o valor da conta de água: ");
+        System.out.print("Conta de água: ");
         float contaAgua = entrada.nextFloat();
 
-        System.out.print("Informe o valor da conta de telefone: ");
+        System.out.print("Conta de telefone: ");
         float contaTelefone = entrada.nextFloat();
 
-        System.out.print("Informe o valor da escola do filho: ");
+        System.out.print("Escola do filho: ");
         float escolaFilho = entrada.nextFloat();
 
-        System.out.print("Informe o valor da fatura do cartão: ");
+        System.out.print("Fatura do cartão: ");
         float faturaCartao = entrada.nextFloat();
 
-        System.out.print("Informe o valor gasto no supermercado: ");
+        System.out.print("Gasto no supermercado: ");
         float gastoSupermercado = entrada.nextFloat();
-
-        float somaTotalGastos = 0.0F;
 
         somaTotalGastos += contaLuz;
         somaTotalGastos += contaAgua;
@@ -34,7 +35,7 @@ public class GastoFamiliar2 {
         somaTotalGastos += gastoSupermercado;
 
         System.out.println("\nGasto total: R$" + somaTotalGastos);
-        
+
         entrada.close();
     }
 }
