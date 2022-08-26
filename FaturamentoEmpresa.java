78421b745c417595aa18bf150a9211e008cd64ed
João V S Pessin <67722165+joaopessin@users.noreply.github.com>
2022-08-26 20:52:56 -0300

    Add files via upload

---
+++ FaturamentoEmpresa.java
@@ -0,0 +1,26 @@
+package _32_logica_5_4;
+
+import java.util.Scanner;
+
+public class FaturamentoEmpresa {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        float metaAnual = 100000.0F, faturamentoReal = 0.0F, mediaSalarial = 1425.5F,
+                aumentoSalarial = mediaSalarial + (mediaSalarial / 100) * 15.75F,
+                quebraSalarial = (mediaSalarial / 100) * 80, salarioMinimo = (mediaSalarial / 100) * 65;
+
+        System.out.print("Informe o faturamento anual da empresa: ");
+        faturamentoReal = entrada.nextFloat();
+
+        if(faturamentoReal >= metaAnual) {
+            mediaSalarial = aumentoSalarial;
+        } else if(faturamentoReal >= (metaAnual / 100) * 80) {
+            mediaSalarial = quebraSalarial;
+        } else {
+            mediaSalarial = salarioMinimo;
+        }
+
+        System.out.print("\nMédia salarial dos funcionários: \nR$" + mediaSalarial);
+    }
+}
