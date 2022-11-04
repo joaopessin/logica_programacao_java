5ca5d88736e16cce78e71b7a4a07b92e14e1c57f
João V S Pessin <67722165+joaopessin@users.noreply.github.com>
2022-11-04 18:15:59 -0300

    Update Principal_28_1.java

--- Principal_28_1.java
+++ Principal_28_1.java
@@ -1,24 +1,30 @@
-package _6_desafios;
-
-public class Principal_28_1 {
+public class Principal {
     public static void main(String[] args) {
-        Fornecedor_28_3 imobiliaria = new Fornecedor_28_3();
+        Fornecedor imobiliaria = new Fornecedor();
         imobiliaria.setNome("Casa & Cia Negócios Imobiliários");
 
-        Fornecedor_28_3 mercado = new Fornecedor_28_3();
+        Fornecedor mercado = new Fornecedor();
         mercado.setNome("Mercado do João");
 
-        ContaPagar_28_2 conta1 = new ContaPagar_28_2();
+        ContaPagar conta1 = new ContaPagar();
         conta1.setDescricao("Aluguel da matriz");
         conta1.setValor(1230d);
         conta1.setDataVencimento("10/05/2012");
         conta1.setFornecedor(imobiliaria);
 
-        ContaPagar_28_2 conta2 = new ContaPagar_28_2(mercado, "Compras do mês", 390d, "19/05/2012");
-        ContaPagar_28_2 conta3 = new ContaPagar_28_2(mercado, "Aluguel da filial", 700d, "11/05/2012");
+        ContaPagar conta2 = new ContaPagar(mercado, "Compras do mês", 390d, "19/05/2012");
 
+        ContaPagar conta3 = new ContaPagar(mercado, "Aluguel da filial", 700d, "11/05/2012");
+
+        // pagamento de conta pendente (ok, deve funcionar)
         conta1.pagar();
+
+        // tentativa de pagar uma conta cancelada (não deve aceitar pagamento)
+        conta2.cancelar();
         conta2.pagar();
+
+        // tentativa de pagar uma conta duas vezes (não deve aceitar na segunda vez)
+        conta3.pagar();
         conta3.pagar();
     }
 }
