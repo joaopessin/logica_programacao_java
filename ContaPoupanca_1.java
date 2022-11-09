d927980b53704bd3bd5b1708f01508c23cba0626
João V S Pessin <67722165+joaopessin@users.noreply.github.com>
2022-11-08 21:12:35 -0300

    Add files via upload

---
+++ ContaPoupanca_1.java
@@ -0,0 +1,6 @@
+package _11_exercises._40_conta_bancaria;
+
+public class ContaPoupanca_1 extends Conta_1 {
+    private double rendimentos = 30;
+    public double getRendimentos() { return rendimentos; }
+}
