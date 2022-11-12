dc036566fd6503e3051c311949cb918dbd39a435
João V S Pessin <jv.pessin20@gmail.com>
2022-11-12 15:37:33 -0300

    added 32_conta_a_pagar finished version and rename packages

--- Pessoa.java
+++
@@ -1,8 +0,0 @@
-package _11_exercises._43_desafio_4_2;
-
-public abstract class Pessoa {
-    public String nome;
-
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-}
---
+++ _32_3_conta_a_pagar/Pessoa.java
@@ -0,0 +1,8 @@
+package _11_exercises._43_desafio_4_2;
+
+public abstract class Pessoa {
+    public String nome;
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+}
