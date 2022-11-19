004c7489cfc01402cd4417d282cfae97b6dc54af
ALUNO CETEC <aluno@labcetec.net>
2022-11-19 08:24:41 -0300

    added _34_exercicio 2.0 version unfinished

---
+++ _34_2_exercicio_interface/impressao/impressoras/ImpressoraHP.java
@@ -0,0 +1,4 @@
+package _11_exercises._47_2_exercicio_interface.impressao.impressoras;
+
+public class ImpressoraHP {
+}
---
+++ _34_2_exercicio_interface/pagamento/Autorizavel.java
@@ -0,0 +1,4 @@
+package _11_exercises._47_2_exercicio_interface.pagamento;
+
+public interface Autorizavel {
+}
