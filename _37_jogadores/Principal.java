6b4321602312545119c73e5ef76c5a3c249b64a0
ALUNO CETEC <aluno@labcetec.net>
2022-11-25 21:27:48 -0300

    added _37_jogadores 1.0 version

---
+++ _37_jogadores/Principal.java
@@ -0,0 +1,45 @@
+package _11_exercises._49_1_jogadores;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String nomesJogadores[] = new String[2];
+
+        System.out.println("\n---------- CADASTRAR JOGADORES ----------");
+        for (int i = 0; i < nomesJogadores.length; i++) {
+            System.out.print("Informe o nome do " + (i + 1) + "º jogador: ");
+            nomesJogadores[i] = scanner.nextLine();
+        }
+
+        // Gravar arquivo
+        try {
+            FileWriter arquivo = new FileWriter("C:\\Users\\aluno\\Documents\\João\\Java\\src\\_11_exercises\\_49_jogadores\\nomes_jogadores");
+            PrintWriter gravarArquivos = new PrintWriter(arquivo);
+
+            for (int i = 0; i < nomesJogadores.length; i++) {
+                gravarArquivos.println(nomesJogadores[i]);
+            }
+
+            arquivo.flush();
+            arquivo.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Ler arquivo
+        FileReader dados = new FileReader("C:\\Users\\aluno\\Documents\\João\\Java\\src\\_11_exercises\\_49_jogadores\\nomes_jogadores");
+        BufferedReader conteudo = new BufferedReader(dados);
+
+        System.out.println("\n---------- LISTAR JOGADORES ----------");
+        String linha = conteudo.readLine(); // Ler primeira linha
+        while (linha != null){
+            System.out.println(linha);
+            linha = conteudo.readLine();    // Ler a próxima linha
+        }
+
+        dados.close();
+    }
+}
